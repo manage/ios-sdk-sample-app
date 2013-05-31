8bebc7c7f35490d0815414595b428f485ce41997
Mike Sergio <mikeserg@gmail.com>
2013-05-31 16:35:03 -0700

    Initial Commit

---
+++ AppSponserTest/ASTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASTAppDelegate.h
+//  AppSponserTest
+//
+//  Created by Mike Sergio on 5/31/13.
+//  Copyright (c) 2013 AST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppSponserTest/ASTViewController.m
@@ -0,0 +1,29 @@
+//
+//  ASTViewController.m
+//  AppSponserTest
+//
+//  Created by Mike Sergio on 5/31/13.
+//  Copyright (c) 2013 AST. All rights reserved.
+//
+
+#import "ASTViewController.h"
+
+@interface ASTViewController ()
+
+@end
+
+@implementation ASTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
